c566a56eb654edac616665a63dda244825143c2f
Hrach <steepgaskin@gmail.com>
2021-07-13 14:44:17 +0400

    task component v1

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,22 @@
 <template>
+  <div>
+    <Task task="Image optimization, minify JS/CSS/HTML"/>
+  </div>
 </template>
 
 <script>
 
+import Task from "@/components/Task";
 export default {
   name: 'App',
   components: {
+    Task
   }
 }
 </script>
 
 <style>
-
+body {
+  font-family: 'Roboto', sans-serif;
+}
 </style>
---
+++ src/components/Task.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="task-box">
+    <p class="task">{{task}}</p>
+    <div class="options">
+      <img src="../assets/more.svg" alt="more" width="16">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Task",
+  data: () => ({
+  }),
+  props: {
+    task: {
+      require: true,
+      type: String,
+    }
+  },
+}
+</script>
+
+<style scoped>
+.task-box {
+  border: 1px solid #e4e4e7;
+  padding: 16px 20px;
+  border-radius: 4px;
+  width: 282px;
+  color: #868f9c;
+  font-weight: normal;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  cursor: pointer;
+  box-shadow: 0.5px 2px 14px 0px #e4e4e4;
+  transition: 0.2s;
+}
+
+.task-box:hover {
+  box-shadow: 1px 8px 14px 0px #e4e4e4;
+}
+
+.task {
+  width: 260px;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  margin: 0;
+}
+
+.options {
+
+}
+</style>
